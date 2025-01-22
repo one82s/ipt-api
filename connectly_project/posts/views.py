d895abab27eb37f46f7dfa4e6a35cebe81afed43
Juanito Ortega <one82s@yahoo.com>
2025-01-22 19:27:49 +0800

    Password encryption

--- connectly_project/posts/views.py
+++ connectly_project/posts/views.py
@@ -4,17 +4,43 @@
 # from django.shortcuts import render
 # from django.http import JsonResponse
 # from .models import User
+import bcrypt
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User, Post, Comment
 from .serializers import UserSerializer, PostSerializer, CommentSerializer
+from django.contrib.auth.hashers import make_password, check_password 
 
 
 class UserListCreate(APIView):
+     
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
+        
+        hashed_password = make_password("mypassword123")
+        print(hashed_password)  # Outputs a hashed version of the password
+
+       
+        # Verifying the hashed password
+        isPasswordValid = check_password("mypassword123", hashed_password)
+        print('Is the password valid? ', isPasswordValid)  # Outputs True if the password matches
+
+         #Salting
+        password = b'password1234'
+        salt = bcrypt.gensalt()
+        #hashWithSaltPassword = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+        hashWithSaltPassword = bcrypt.hashpw(password, salt)
+        print('Hash with salt password is: ', hashWithSaltPassword)
+        # Verify a password
+        passwordToVerify = b'password1234'
+
+        if bcrypt.checkpw(passwordToVerify, hashWithSaltPassword):
+            print("Password is correct")
+        else:
+            print("Invalid password")
+
         return Response(serializer.data)
 
 
