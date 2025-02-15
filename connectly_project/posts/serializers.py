553ca1a2b56183a110da69d666e78c1a956a0d46
Juanito Ortega <one82s@yahoo.com>
2025-02-15 18:02:14 +0800

    Remove comments dependency

--- connectly_project/posts/serializers.py
+++ connectly_project/posts/serializers.py
@@ -14,7 +14,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id', 'content', 'author', 'created_at', 'comments']
+        fields = ['id', 'content', 'author', 'created_at']
 
 
 class CommentSerializer(serializers.ModelSerializer):
