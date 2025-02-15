e8fa1eea4abb1dc6a8bcffde525b821db2b356db
Juanito Ortega <one82s@yahoo.com>
2025-02-15 15:02:59 +0800

    JWT implementation

--- connectly_project/posts/urls.py
+++ connectly_project/posts/urls.py
@@ -10,10 +10,13 @@
 
 from django.urls import path
 from .views import UserListCreate, PostListCreate, CommentListCreate
+from rest_framework_simplejwt.views import (TokenRefreshView, TokenObtainPairView)
 
 
 urlpatterns = [
     path('users/', UserListCreate.as_view(), name='user-list-create'),
     path('posts/', PostListCreate.as_view(), name='post-list-create'),
     path('comments/', CommentListCreate.as_view(), name='comment-list-create'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
